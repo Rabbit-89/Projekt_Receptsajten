3e5836cc15bc9829a7daa1d87bda5fe9e5c13df2
Suzan Al-Majdalawi <suzanmajdalawi1@gmail.com>
2025-11-11 19:12:07 +0100

    6_lazzy loading _import only one file from views files not all..best for page loading time (Network)

--- vue-project/src/routes/index.js
+++ vue-project/src/routes/index.js
@@ -1,17 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'    // createHashWebHistory vissa /# best createWebHistory
 
-import Home from '../views/Home.vue'
+/*import Home from '../views/Home.vue'
 import Recipe from '../views/Recipe.vue'
 import Recipes from '../views/Recipes.vue'
 import Category from '../views/Category.vue'
-import Categories from '../views/Categories.vue'
+import Categories from '../views/Categories.vue'*/
+
+const Home = () => import('../views/Home.vue');
+// const Recipe = () => import('../views/Recipe.vue'); ----skriva i samma line 17 bredvid component för lazzy loading 
+// const Recipes = () => import('../views/Recipes.vue');
+// const Category = () => import('../views/Category.vue');
+// const Categories = () => import('../views/Categories.vue');
 
 const routes = [
     { path: "/", component: Home },
-    { path: "/recipe", component: Recipe },
-    { path: "/recipes", component: Recipes }, // Alla Recipe
-    { path: "/category", component: Category },
-    { path: "/categories", component: Categories }  // Alla Category
+    { path: "/recipe", component: () => import('../views/Recipe.vue') },   // skriva () => import('../views/Recipe.vue') istället Recipe ..the same
+    { path: "/recipes", component: () => import('../views/Recipes.vue') }, // Alla Recipe
+    { path: "/category", component: () => import('../views/Category.vue') },
+    { path: "/categories", component: () => import('../views/Categories.vue') }  // Alla Category
 ];
 
 const router = createRouter({
