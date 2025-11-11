ec3d438d3e0d9450f6a8304b2251fac50cd15636
Suzan Al-Majdalawi <suzanmajdalawi1@gmail.com>
2025-11-11 21:33:33 +0100

    9_fetch('http://localhost:3000/recipes')_inte byta port från 3000  till 8000,för att featch apiRecipe och vissa i /recipes

--- vue-project/src/views/Recipes.vue
+++ vue-project/src/views/Recipes.vue
@@ -1,9 +1,23 @@
 <script setup>
+import { ref, onMounted } from 'vue'
 
+const recipeList = ref([]);
+
+onMounted(() => {
+      fetch('http://localhost:3000/recipes')
+      .then((response) => response.json())
+      .then(apiRecipes => {
+        recipeList.value = apiRecipes
+      })
+      .catch(err => console.error('Fetch error:', err));
+})
 </script>
 
 <template>
-  <h1>All Recipe</h1>
+  <h1>
+    <pre>{{ recipeList }}</pre>
+  </h1>
+
 </template>
 
 <style scoped>
