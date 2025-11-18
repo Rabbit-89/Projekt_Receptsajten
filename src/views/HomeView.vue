0cb91589477e10eba3306e7e2a74cb85d7fe5b3c
elinfelicia <elinratcliff@icloud.com>
2025-11-18 11:03:29 +0100

    add recipe card component and create cards. clickability tba

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+import RecipeCard from '../components/RecipeCard.vue'
+import recipesData from '../data/recipes.json'
 
+const recipes = ref([])
+
+onMounted(() => {
+  recipes.value = recipesData
+})
 </script>
 
 <template>
   <main>
-    
+    <div class="recipe-list">
+      <RecipeCard v-for="recipe in recipes" :key="recipe.id" :recipe="recipe" />
+    </div>
   </main>
 </template>
+
+<style scoped>
+
+</style>
