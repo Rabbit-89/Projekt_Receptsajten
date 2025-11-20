4dc4b2a17e76f50c5e9cfe87b331871cb9244ec5
Suzan Al-Majdalawi <suzanmajdalawi1@gmail.com>
2025-11-20 18:58:15 +0100

    #6_2_import Footer i App file and remove RouterLink not use

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import Header from './components/Header.vue';
+import { RouterView } from "vue-router";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 </script>
 
 <template>
   <header>
-<Header/> 
+    <Header />
   </header>
-<main>
- <RouterView />
-</main>
- 
+  <main>
+    <RouterView />
+  </main>
+  <footer>
+    <Footer />
+  </footer>
 </template>
 
 <style scoped>
-
-body{
+body {
   margin: 2rem;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -87,5 +89,4 @@ nav a:first-of-type {
     margin-top: 1rem;
   }
 } */
-
 </style>
