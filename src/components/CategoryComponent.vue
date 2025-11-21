c78bee909fd85ebadc6b641c113fa1753fc57f7b
Suzan Al-Majdalawi <suzanmajdalawi1@gmail.com>
2025-11-21 22:23:09 +0100

    2_fix categoryComponent and NavCategory with count recipe in category  and image can see number + name of category

---
+++ src/components/CategoryComponent.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+  name: String,
+  slug: String,
+  image: String,
+  count: Number
+});
+</script>
+
+<template>
+  <router-link
+    :to="{ name: 'category', params: { categoryId: slug } }"
+    class="category-item"
+  >
+    <img :src="image" alt="" class="category-icon" />
+    <span class="category-name">{{ name }} ({{ count }})</span>
+  </router-link>
+</template>
+
+<style scoped>
+.category-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-decoration: none;
+  gap: 8px;
+  padding: 10px;
+}
+
+.category-icon {
+  width: 80px;
+  height: 80px;
+  object-fit: cover;
+  border-radius: 50%;
+}
+
+.category-name {
+  font-size: 1.1rem;
+  color: #333;
+  font-weight: 500;
+}
+</style>
+
---
+++ src/components/NavCategory.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { computed } from 'vue';
+import recipes from '../data/recipes.json';
+import CategoryComponent from './CategoryComponent.vue'
+
+// Skapa unika kategorier med count
+const categories = computed(() => {
+  const map = new Map();
+
+  recipes.forEach(r => {
+    if (!map.has(r.categorySlug)) {
+      map.set(r.categorySlug, {
+        id: r.categoryId,
+        name: r.categorySlug.charAt(0).toUpperCase() + r.categorySlug.slice(1),
+        slug: r.categorySlug,
+        image: r.categoryImage,
+        count: 1
+      });
+    } else {
+      map.get(r.categorySlug).count++;
+    }
+  });
+
+  return [...map.values()];
+});
+</script>
+
+<template>
+  <section class="category-box">
+    <h1>Categories</h1>
+
+    <div class="category-nav">
+      <CategoryComponent
+        v-for="cat in categories"
+        :key="cat.slug"
+        :name="cat.name"
+        :slug="cat.slug"
+        :image="cat.image"
+        :count="cat.count"
+      />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.category-box {
+  background-color: #f7ce78;
+  padding: 20px;
+  width: 100%;
+}
+
+.category-nav {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 18px;
+}
+</style>
