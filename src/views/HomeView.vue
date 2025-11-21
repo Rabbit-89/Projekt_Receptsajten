7d6dc8ebf7c4cde99fc2998ec97b2f79ccea40e1
Suzan Al-Majdalawi <suzanmajdalawi1@gmail.com>
2025-11-21 22:33:01 +0100

    4_fix UX and style class recipe-list responsiv..and change categoryNav to new NavCategory

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
 import { ref, onMounted } from 'vue'
 import RecipeCard from '../components/RecipeCard.vue'
 import recipesData from '../data/recipes.json'
-import CategoryNav from '@/components/CategoryNav.vue';
+import NavCategory from '@/components/NavCategory.vue';
 
 const recipes = ref([])
 
@@ -13,22 +13,51 @@ onMounted(() => {
 
 <template>
   <main>
-    <CategoryNav/>
+    <NavCategory/>
+    
     <div class="recipe-list">
-      <RecipeCard v-for="recipe in recipes" :key="recipe.id" :recipe="recipe" />
+      <RecipeCard 
+        v-for="recipe in recipes" 
+        :key="recipe.id" 
+        :recipe="recipe" 
+      />
     </div>
   </main>
-
- 
 </template>
-
-<style scoped>
-
-</style>
 
 <style scoped>
 main {
   display: flex;
   flex-direction: column;
+  gap: 30px;
+  padding: 20px;
+}
+
+.recipe-list {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+  gap: 20px;
+}
+
+/* Optional: add some padding to RecipeCard for mobile */
+@media screen and (max-width: 600px) {
+  .recipe-list {
+    grid-template-columns: 1fr; /* single column on small screens */
+    gap: 15px;
   }
+}
+
+@media screen and (min-width: 601px) and (max-width: 1024px) {
+  .recipe-list {
+    grid-template-columns: repeat(2, 1fr); /* 2 columns for tablets */
+    gap: 20px;
+  }
+}
+
+@media screen and (min-width: 1025px) {
+  .recipe-list {
+    grid-template-columns: repeat(3, 1fr); /* 3 columns for large screens */
+    gap: 25px;
+  }
+}
 </style>
