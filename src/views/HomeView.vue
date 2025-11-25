989078ab6b41bb1d9f39c2f0b8f481a0e85dceea
NancySamefors <nancysamefors@icloud.com>
2025-11-25 14:16:31 +0100

    Fixat cayegory view samt brödsmulor då det stod category istället för categories i filen RecipeView.vue i views mappen vilket gjorde att alla länkar inte fungerade först

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,7 +15,7 @@ onMounted(() => {
   <main class="home-container">
     <CategoryNav />
 
-    <h1 class="page-title">Alla Recept</h1>
+    <h1 class="page-title">All Recipes</h1>
 
     <div class="recipe-grid"> 
     <RecipeCard 
